2a8cb40664dcef5583858912812dfcd7559c18c0
Tyler Peterson <tylermpeterson36@gmail.com>
2016-03-05 08:25:18 -0700

    chat styling

--- public/js/chat2.js
+++ public/js/chat2.js
@@ -6,17 +6,17 @@ $(document).ready(function(){
 		say: function(text) {
 			//$('#chatarea').append('<div id="typed-strings" class="well chatbox robot-chat"><p>Robot: '+ text +'</p></div>');
 			setTimeout(function(){ 
-				$('<div class="well chatbox robot-chat">You: '+ text +'</div>').hide().appendTo("#chatarea").fadeIn(2000);
+				$('<div class="well chatbox robot-chat">Robot: '+ text +'</div>').hide().appendTo("#chatarea").fadeIn(2000);
+
 			 }, 750);
 		
-
+			window.scrollTo(0,document.body.scrollHeight);
 			// $('#chatarea').append('<span id="typed" class="well chatbox robot-chat" style="display:block;"></span>');
 			// $("#typed").typed({
 	  //           stringsElement: $('#typed-strings'),
 	  //           typeSpeed: -25
    //      	});
    // Code above is for the typed.js. Only works for the first element, library probably requires the ID of the element.
-			window.scrollTo(0,document.body.scrollHeight);
 		},		
 		startInput: function() {
 			awaitingAnswer = true;
@@ -30,7 +30,7 @@ $(document).ready(function(){
 				e.preventDefault();
 				callback();
 			});
-
+			window.scrollTo(0,document.body.scrollHeight);
 		}
 	}
 	var form = new basicForm(botHandle);
@@ -40,10 +40,11 @@ $(document).ready(function(){
 			var text = $('#user-input').val();
 			document.getElementById("user-input").value = "";
 			//$('#chatarea').append('<div class="well chatbox user-chat">You: '+ text +'</div>');
-			$('<div class="well chatbox user-chat">You: '+ text +'</div>').hide().appendTo("#chatarea").fadeIn(1000);
+			$('<div class="well chatbox user-chat"> '+ text +'</div>').hide().appendTo("#chatarea").fadeIn(1000);
 
 			form.handleInput(text);
 		}
+		window.scrollTo(0,document.body.scrollHeight);
 		$('#input-submit').focus();
 	}
 
