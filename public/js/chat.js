c947f6ad0ac85e0aa8366d4c3dd6277b4bb52501
Tyler Peterson <tylermpeterson36@gmail.com>
2016-03-05 03:24:01 -0700

    progress on typed.js

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,35 +1,18 @@
 $(document).ready(function(){
-	
-	var myFirebase = new Firebase("https://luminous-heat-5923.firebaseio.com/")
-	
-	var user = $.ajax({
-		url: '/user'
-	})
-	var responses = {};
 	var form = new basicForm();
-	var lastQuestion;
 	var question = form.getFirstQuestion();
 	var awaitingAnswer = false;
-	
-	var finishChat = function() {
-		console.log("finishing chat")
-		var responseId = Math.random().toString();
-		user.done(function(userData) {
-			myFirebase.child('responses/' + userData._id + '/' + responseId.split('.')[1]).set({
-				formId: form.id,
-				responses: responses
-			});
-		})
-	}
-	
 	var runQuestion = function() {
 		var text = question.questionText;
-		$('#chatarea').append('<div class="well chatbox robot-chat">Robot: '+ text +'</div>');
-		if(question.id == "done") {
-			finishChat()
-		} else {
-			awaitingAnswer = true;
-		}
+		$('#chatarea').append('<div id="typed-strings" class="well chatbox robot-chat"><p>Robot: '+ text +'</p></div>');
+		//$('#chatarea').append('<span id="typed" class="well chatbox robot-chat" style="display:block;"></span>');
+
+        $("#typed").typed({
+            stringsElement: $('#typed-strings'),
+            typeSpeed: -25
+        });
+
+		awaitingAnswer = true;
 	}
 
 	$('#input-submit').click(function(e){
@@ -37,12 +20,7 @@ $(document).ready(function(){
 		var text = $('#user-input').val();
 			awaitingAnswer = false;
 			$('#chatarea').append('<div class="well chatbox user-chat">You: '+ text +'</div>');
-			lastQuestion = question;
 			question = question.acceptInput(text);
-			if(question != lastQuestion) {
-				responses[lastQuestion.id] = text;
-			}
-			
 			runQuestion();
 		}
 	});
