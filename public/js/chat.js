4bd038c90e62f2ccd0bb96e6be92184bcdb1115b
James Larsen <JamesB444@msn.com>
2016-03-05 05:20:42 -0700

    Modified the the chat js file to work more effectively with the convocontroller function

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,12 +1,13 @@
 var onUserPost = [];
 
+var convoIter = 0;
+
 $(document).ready(function(){
-	converse(0);
-	var form = new basicForm();
-	var question = form.getFirstQuestion();
+//	var form = new basicForm();
+//	var question = form.getFirstQuestion();
 	var awaitingAnswer = false;
 	var runQuestion = function() {
-		var text = question.questionText;
+//		var text = question.questionText;
 		// $('#chatarea').append('<div id="typed-strings" class="well chatbox robot-chat"><p>Robot: '+ text +'</p></div>');
 		// //$('#chatarea').append('<span id="typed" class="well chatbox robot-chat" style="display:block;"></span>');
 
@@ -16,23 +17,20 @@ $(document).ready(function(){
   //       });
 
 		awaitingAnswer = true;
-        postMessage(question.questionText);
-		if(question.id == "done") {
-			finishChat()
-		} else {
-			awaitingAnswer = true;
-		}
+        converse(convoIter++);
+  		// if(question.id == "done") {
+		// 	finishChat()
+		// } else {
+		// 	awaitingAnswer = true;
+		// }
 	}
-    
-
-
 
 	$('#input-submit').click(function(e){
 		var text = $('#user-input').val();
 		if(awaitingAnswer) {
 			awaitingAnswer = false;
 			$('#chatarea').append('<div class="well chatbox user-chat">You: '+ text +'</div>');
-			question = question.acceptInput(text);
+//			question = question.acceptInput(text);
 			runQuestion();
 		}
         for(var i = 0; i < onUserPost.length; ++i)
