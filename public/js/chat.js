5799e4af0fe9cb0fc2a8226e34edad94c3e52e87
bravefoot <b.v.campbell3@gmail.com>
2016-03-05 01:48:16 -0700

    Chat bot saves responses

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,11 +1,35 @@
 $(document).ready(function(){
+	
+	var myFirebase = new Firebase("https://luminous-heat-5923.firebaseio.com/")
+	
+	var user = $.ajax({
+		url: '/user'
+	})
+	var responses = {};
 	var form = new basicForm();
+	var lastQuestion;
 	var question = form.getFirstQuestion();
 	var awaitingAnswer = false;
+	
+	var finishChat = function() {
+		console.log("finishing chat")
+		var responseId = Math.random().toString();
+		user.done(function(userData) {
+			myFirebase.child('responses/' + userData._id + '/' + responseId.split('.')[1]).set({
+				formId: form.id,
+				responses: responses
+			});
+		})
+	}
+	
 	var runQuestion = function() {
 		var text = question.questionText;
 		$('#chatarea').append('<div>Robot: '+ text +'</div>');
-		awaitingAnswer = true;
+		if(question.id == "done") {
+			finishChat()
+		} else {
+			awaitingAnswer = true;
+		}
 	}
 	
 	$('#input-submit').click(function(e){
@@ -13,9 +37,15 @@ $(document).ready(function(){
 		var text = $('#user-input').val();
 			awaitingAnswer = false;
 			$('#chatarea').append('<div>You: '+ text +'</div>');
+			lastQuestion = question;
 			question = question.acceptInput(text);
+			if(question != lastQuestion) {
+				responses[lastQuestion.id] = text;
+			}
+			
 			runQuestion();
 		}
 	});
+	
 	runQuestion();
 });
